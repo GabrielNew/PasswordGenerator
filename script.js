3e21be2abc3b4d88d6b21d02806541e7ffbc6ff8
GabrielNew <gabrielfai70@gmail.com>
2024-04-05 21:42:57 -0300

    Lógica da criação da senha

---
+++ script.js
@@ -0,0 +1,25 @@
+let passwordLenghtRange = 8
+
+const generatePassword = () =>
+{
+    const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*()[]"
+    let password = ""
+
+    for(let i=0; i<passwordLenghtRange; i++)
+    {
+        const randomNumber = Math.floor(Math.random() * chars.length)
+
+        password += chars.substring(randomNumber, randomNumber+1)
+    }
+
+    const passwordInput = document.querySelector("#password")
+    passwordInput.value = password;
+
+    const passwordLenghtRangeEl = document.querySelector('#password-length')
+
+    passwordLenghtRangeEl.addEventListener('input', function() {
+        passwordLenghtRange = passwordLenghtRangeEl.value
+        generatePassword()
+        console.log('entrei aqui')
+    })
+}
